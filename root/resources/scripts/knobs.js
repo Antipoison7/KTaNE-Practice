4f10fd4a149a0590b053bcf939ba121399803c58
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-27 19:48:51 +1000

    Added spinning functionality

--- root/resources/scripts/knobs.js
+++ root/resources/scripts/knobs.js
@@ -1,4 +1,6 @@
 document.getElementById("submitButton").addEventListener("click", isCorrect);
+document.getElementById("arrow").addEventListener("click", rotateKnob);
+let arrowDir = 0;
 
 function isCorrect()
 {
@@ -8,4 +10,20 @@ function isCorrect()
 function generateProblem()
 {
 
+}
+
+function rotateKnob()
+{
+    const arrow = document.getElementById("arrow");
+
+    if(arrowDir <4)
+    {
+        arrowDir++;
+    }
+    else
+    {
+        arrowDir = 0;
+    }
+
+    arrow.style.transform = "rotate("+(arrowDir*90)+"deg)";
 }
