33b2af83f6be2b5b19b9dcb0f2d81ffcb42a08f4
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-27 19:59:23 +1000

    Created generator for all problems - knob

--- root/resources/scripts/knobs.js
+++ root/resources/scripts/knobs.js
@@ -16,7 +16,7 @@ function rotateKnob()
 {
     const arrow = document.getElementById("arrow");
 
-    if(arrowDir <4)
+    if(arrowDir <3)
     {
         arrowDir++;
     }
@@ -26,4 +26,38 @@ function rotateKnob()
     }
 
     arrow.style.transform = "rotate("+(arrowDir*90)+"deg)";
+}
+
+function listAllProblems()
+{
+    let problemArr = [];
+
+    // Up
+    problemArr.push({solve: 0, topRow: [0,0,1,0,1,1], 
+                            bottomRow: [1,1,1,1,0,1]});
+
+    problemArr.push({solve: 0, topRow: [1,0,1,0,1,0], 
+                            bottomRow: [0,1,1,0,1,1]});
+    // Right
+    problemArr.push({solve: 1, topRow: [1,0,1,1,1,1], 
+                            bottomRow: [1,1,1,0,1,0]});
+
+    problemArr.push({solve: 1, topRow: [1,0,1,1,0,0], 
+                            bottomRow: [1,1,1,0,1,0]});
+
+    // Down
+    problemArr.push({solve: 2, topRow: [0,1,1,0,0,1], 
+                            bottomRow: [1,1,1,1,0,1]});
+
+    problemArr.push({solve: 2, topRow: [1,0,1,0,1,0], 
+                            bottomRow: [0,1,0,0,0,1]});
+
+    // Left
+    problemArr.push({solve: 3, topRow: [0,0,0,0,1,0], 
+                            bottomRow: [1,0,0,1,1,1]});
+
+    problemArr.push({solve: 3, topRow: [0,0,0,0,1,0], 
+                            bottomRow: [0,0,0,1,1,0]});
+
+    return(problemArr);
 }
