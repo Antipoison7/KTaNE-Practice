6c55f1086131133fb5d97835c75ea3422194f4f5
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-28 08:34:47 +1000

    Populates answers div on clicking submit.

--- root/resources/scripts/knobs.js
+++ root/resources/scripts/knobs.js
@@ -2,12 +2,24 @@ document.getElementById("submitButton").addEventListener("click", isCorrect);
 document.getElementById("arrow").addEventListener("click", rotateKnob);
 let arrowDir = 0;
 let currAnswer = 0;
+let currProblem = 0;
 let numberOfSolves = 0;
+let prevAttempts = [];
 
 generateProblem();
 
 function isCorrect()
 {
+    prevAttempts.unshift(
+        {
+            index: prevAttempts.length,
+            inputDir: arrowDir,
+            correctDir: currAnswer,
+            problem: currProblem
+        });
+
+    showAnswers();
+
     if(arrowDir == currAnswer)
         {
             console.log("Correct");
@@ -31,10 +43,10 @@ function isCorrect()
 function generateProblem()
 {
     let problems = listAllProblems();
-    let problem = problems[Math.floor(Math.random()*problems.length)]
-    let combinedLEDs = problem.topRow.concat(problem.bottomRow);
+    let currProblem = problems[Math.floor(Math.random()*problems.length)]
+    let combinedLEDs = currProblem.topRow.concat(currProblem.bottomRow);
 
-    currAnswer = problem.solve;
+    currAnswer = currProblem.solve;
 
     let lights = [];
 
@@ -51,9 +63,6 @@ function generateProblem()
     lights.push(document.getElementById("light11"));
     lights.push(document.getElementById("light10"));
 
-    console.log(problem.topRow);
-    console.log(problem.bottomRow);
-
     let counter = 0;
     for(let element of lights) {
         if(combinedLEDs[counter] == 1)
@@ -68,6 +77,72 @@ function generateProblem()
         }
         counter++;
     };
+}
+
+function showExplanation(indexVal)
+{
+    if(prevAttempts[indexVal].inputDir == prevAttempts[indexVal].correctDir)
+    {
+        
+    }
+    else
+    {
+
+    }
+}
+
+function showAnswers()
+{
+    let divBox = document.getElementById("prevAttempts");
+
+    let innerString = "";
+
+    let flipArray = prevAttempts.map(obj => ({...obj, index: obj.index, inputDir: obj.inputDir, correctDir: obj.correctDir, problem: obj.problem}));
+    flipArray;
+
+    flipArray.forEach(element => {
+        innerString += "<li class=\"" + ((element.inputDir == element.correctDir)? "correctAnsw": "falseAnsw") + "\" onclick=\"showExplanation(" + element.index + ")\">";
+
+        innerString += "Your Answer: ";
+
+        switch (element.inputDir)
+        {
+            case 0:
+                innerString += "Up";
+                break;
+            case 1:
+                innerString += "Right";
+                break;
+            case 2:
+                innerString += "Down";
+                break;
+            case 3:
+                innerString += "Left";
+                break;
+        }
+
+        innerString += " Correct Answer: ";
+
+        switch (element.correctDir)
+        {
+            case 0:
+                innerString += "Up";
+                break;
+            case 1:
+                innerString += "Right";
+                break;
+            case 2:
+                innerString += "Down";
+                break;
+            case 3:
+                innerString += "Left";
+                break;
+        }
+
+        innerString += "</li>";
+    });
+
+    divBox.innerHTML = innerString;
 }
 
 function rotateKnob()
